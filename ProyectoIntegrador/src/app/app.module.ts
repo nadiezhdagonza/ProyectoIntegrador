b1d1e2cbd1e8b75700de304f63daac15c555c023
nadiezhdagonza <nadiezhda@gmail.com>
2023-05-11 09:46:12 -0300

    2

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent,
+    AcercadeComponent,
+    EstudiosComponent
+    
   ],
   imports: [
     BrowserModule,
